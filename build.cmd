78afb4268d08678e08b874730cee84268407be0e
Marcos Fehlauer <marcosfehlauer@gmail.com>
2025-03-25 14:34:17 -0300

    Fix windows build (Batch is retarded)

--- build.cmd
+++ build.cmd
@@ -2,8 +2,9 @@
 
 REM clang Build version (recommended)
 clang -Os -std=c++20 -Wall -Wextra -fno-strict-aliasing -fwrapv -o main.exe main.cpp core\core.cpp kielo.cpp
+if %errorlevel% neq 0 exit /b %errorlevel%
 
 REM cl Build version
 REM cl /nologo /std:c++20 /Os /EHsc /GR /W4 /Femain.exe main.cpp core\core.cpp kielo.cpp
-
+if %errorlevel% neq 0 exit /b %errorlevel%
 
